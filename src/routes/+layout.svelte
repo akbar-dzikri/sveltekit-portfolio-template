478c97b8cb3a1949bb747810e6729d7fcf3920a6
Akbar Dzikri <akbardzkr05@gmail.com>
2024-12-20 02:33:56 +0700

    changed settings location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,34 @@
 <script lang="ts">
-    import { initializeStores, AppShell } from '@skeletonlabs/skeleton';
-    import * as settings from '$lib/content/settings.json';
-    import Navbar from '$lib/components/Navbar.svelte';
-    import Footer from '$lib/components/Footer.svelte';
-    import '../app.css';
+	import { initializeStores, AppShell } from '@skeletonlabs/skeleton';
+	import * as settings from '$lib/content/setting/settings.json';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '../app.css';
 
-    initializeStores();
-
+	initializeStores();
 </script>
 
 <svelte:head>
-    <title>{settings.title}</title>
-    <meta name="description" content={settings.description} />
-
+	<title>{settings.title}</title>
+	<meta name="description" content={settings.description} />
 </svelte:head>
 
 <AppShell>
-    <Navbar />
-    
-    <main class="container mx-auto px-4 min-h-[calc(100vh-64px-80px)]">
-        <slot/>
-    </main>
+	<Navbar />
 
-    <svelte:fragment slot="footer">
-        <Footer />
-    </svelte:fragment>
+	<main class="container mx-auto min-h-[calc(100vh-64px-80px)] px-4">
+		<slot />
+	</main>
+
+	<svelte:fragment slot="footer">
+		<Footer />
+	</svelte:fragment>
 </AppShell>
 
 <style>
-    :global(body) {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
+	:global(body) {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
 </style>
